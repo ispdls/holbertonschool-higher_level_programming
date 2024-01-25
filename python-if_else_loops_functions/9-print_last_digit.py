2cd354b105094a3d8610ab0cfbb584564ff8706e
ispdls <is.pdls90@gmail.com>
2024-01-25 00:14:55 -0400

    function the prints the last number of a number

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    temp = abs(number) % 10
-    print("{}".format(temp))
+    if number >= 0:
+        temp = abs(number) % 10
+    else:
+        temp = abs(number) % 10 * -1
+
+    print(temp, end='')
+    return temp
