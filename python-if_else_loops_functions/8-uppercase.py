848b16a2e9521e0b70e0539d3dbdb71d0004f8d3
ispdls <is.pdls90@gmail.com>
2024-01-24 23:36:01 -0400

    prints a string in uppercase followed by a new line

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for temp in str:
+        char_temp = ord(temp)
+        if char_temp >= ord('a') and char_temp < ord('z'):
+            char_temp = char_temp - (ord('a') - ord('A'))
+        print(f"{chr(char_temp)}", end='')
+    print()
