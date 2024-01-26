ed35c0a2697fc997bd217c81f7a04885ee4f376c
ispdls <is.pdls90@gmail.com>
2024-01-26 01:28:14 -0400

    FizzBUzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             break
 
         elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end=' ')
 
         elif i % 3 == 0:
             print("Fizz", end=' ')
