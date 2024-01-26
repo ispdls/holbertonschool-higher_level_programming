c15f9e7db946c58d4b39631504321b1aa343ae20
ispdls <is.pdls90@gmail.com>
2024-01-26 04:33:12 -0400

    count the argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argument_count = len(sys.argv) - 1
+
+    if argument_count > 0:
+        print(f"{argument_count} arguments")
+        for i in range(1, argument_count + 1):
+            print(f"{i}: {sys.argv[i]}")
