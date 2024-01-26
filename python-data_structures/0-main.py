202a082256888cc86c785f5cf6d689488a757780
ispdls <is.pdls90@gmail.com>
2024-01-26 17:26:26 -0400

    function that replaces an element in a list at a specific position without modifying the original list.

--- python-data_structures/0-main.py
+++ python-data_structures/0-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print_list_integer = __import__('0-print_list_integer').print_list_integer
 
-my_list = [111, 22, 3, 24, 5]
+my_list = [111, 2232, 332, 24, 5]
 print_list_integer(my_list)
