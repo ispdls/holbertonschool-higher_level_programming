2371abecaf727196b8957b030d23077a02bf8ec9
ispdls <is.pdls90@gmail.com>
2024-01-26 03:27:03 -0400

    calculation import

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
     b = 5
+    sum_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
 
-    print("{} + {} = {}" .format(a, b, calculator_1.add(a, b)))
-    print("{} - {} = {}" .format(a, b, calculator_1.sub(a, b)))
-    print("{} * {} = {}" .format(a, b, calculator_1.mul(a, b)))
-    print("{} / {} = {}" .format(a, b, calculator_1.div(a, b)))
+    print("{} + {} = {}" .format(a, b, sub_result))
+    print("{} - {} = {}" .format(a, b, sub_result))
+    print("{} * {} = {}" .format(a, b, mul_result))
+    print("{} / {} = {}" .format(a, b, div_result))
