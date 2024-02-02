db040401366bfc6a44257dfe03e490435e0a0f4a
ispdls <is.pdls90@gmail.com>
2024-02-02 19:13:38 -0400

    Instantiation with optional size: def __init__(self, size=0):

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""creates class Square with private instance attribute size"""
+
+
+class Square:
+    """defines class and instantiates private instance attribute size with
+    validation."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+
+            else:
+                self.__size = size
+
+    def area(self):
+        """calculates square area"""
+        return self.__size ** 2
