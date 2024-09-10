2a84b784d8bf5243fcded01747b855d511e4e6e3
ispdls <is.pdls90@gmail.com>
2024-09-09 21:12:37 -0400

    element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,12 +2,12 @@
 def element_at(my_list, idx):
     index = 0
 
-    if idx < 0 :
+    if idx < 0:
         return None
-    
+
     elif idx >= len(my_list):
         return None
-    
+
     else:
         for value in my_list:
             if index == idx:
