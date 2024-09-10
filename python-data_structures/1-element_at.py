af3f06405460657c3872fac11e52c52e9ccd61c2
ispdls <is.pdls90@gmail.com>
2024-09-09 21:09:23 -0400

    pop element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,17 +5,11 @@ def element_at(my_list, idx):
     if idx < 0 :
         return None
     
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     
     else:
         for value in my_list:
             if index == idx:
-                return my_list.pop(idx)
+                return my_list(idx)
             index += 1
-                
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 4
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
