0c2ef91b6ed4ff9e4342f364f322ab056e599149
ispdls <is.pdls90@gmail.com>
2024-09-10 21:55:04 -0400

    remover letter

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,10 +2,11 @@
 def no_c(my_string):
 
     temp_list = list(my_string)
+    temp_lists = []
 
     for i in temp_list:
-        if i == 'c' or i == 'C':
-            temp_list.remove(i)
+        if i != 'c' and i != 'C':
+            temp_lists.append(i)
 
-    my_string = ''.join(temp_list)
+    my_string = ''.join(temp_lists)
     return my_string
