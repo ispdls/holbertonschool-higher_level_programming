4facc1a976ac5263a2075daee01c3fa62eaf57ec
ispdls <is.pdls90@gmail.com>
2024-09-12 19:52:24 -0400

    tuple add

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+
+    temp1 = tuple_a[0] + tuple_b[0]
+    temp2 = tuple_a[1] + tuple_b[1]
+
+    temp_tup = (temp1, temp2)
+
+    return temp_tup
