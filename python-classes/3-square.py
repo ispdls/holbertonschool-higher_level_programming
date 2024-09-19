4d68e30259d5c89829394d1b6ad7debcf1a5acee
ispdls <is.pdls90@gmail.com>
2024-09-18 21:36:17 -0400

    method of area

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Define a class Square."""
+
+
+class Square:
+
+    """Private instance attribute: size"""
+    def __init__(self, size=0):
+
+        """Initialises of size"""
+        self.__size = size
+
+        # Validate if size is an integer
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        # Validate if size is non zero
+        if size < 0:
+            raise TypeError("size must be >= 0")
+
+    # method that returns the current square area
+    def area(self):
+
+        # return the area
+        return self.__size * self.__size
