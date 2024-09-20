33ce3a1a0e4748eabed76dc2d17120b1333981ce
ispdls <is.pdls90@gmail.com>
2024-09-19 21:35:22 -0400

    width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,23 +3,31 @@
 
 
 class Rectangle:
-    """Private instance attribute: widht, height"""
+    """A Rectangle class with attributes  width and height"""
     def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
-        # Validate if width is an integer
-        if not isinstance(width, int):
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        # Validate if width is non zero
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
-        # Validate if height is an integer
-        if not isinstance(height, int):
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        # Validate if height is non zero
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-
-        # Assign validated values
-        self.__width = width
-        self.__height = height
+        self.__height = value
