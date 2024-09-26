4f1ab142ca524c0f7d86e76da8f275559b8b2230
ispdls <is.pdls90@gmail.com>
2024-09-25 20:39:17 -0400

    function that returns the list of available attributes and methods of an object using dir()

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,10 +4,14 @@
 
 
 def lookup(obj):
+
     # temp_list is the return
     temp_list = []
 
     for value in dir(obj):
+
+        # temp_list store the attributes and methods
         temp_list.append(value)
 
+    # Return the list
     return temp_list
