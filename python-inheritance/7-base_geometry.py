96cbbeeb5c2fb7e8b0974d57c9a92c9ce598b9e6
ispdls <is.pdls90@gmail.com>
2024-09-26 20:17:01 -0400

    Public instance method def interger_validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""class BaseGeometry."""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
