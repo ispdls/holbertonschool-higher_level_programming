a72372f81d0e66e8380eff140e97b6df9a328c4e
ispdls <is.pdls90@gmail.com>
2024-09-26 21:34:44 -0400

    class Quare

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module defines classes related to basic geometry.
+
+Classes:
+    Rectangle: Base class.
+    Square: Class inherits Rectangle.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+
+    def __init__(self, size):
+        """Instantiation of class"""
+        super().integer_validator('size', size)
+
+        self.__size = size
+
+    def area(self,):
+        """Returns area
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """string object"""
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
