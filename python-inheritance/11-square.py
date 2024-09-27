c8125b3672004d6248a28c71c1804b2feb01ec2b
ispdls <is.pdls90@gmail.com>
2024-09-26 21:42:22 -0400

    class Quare implemet __str__ method

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -27,4 +27,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """string object"""
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
