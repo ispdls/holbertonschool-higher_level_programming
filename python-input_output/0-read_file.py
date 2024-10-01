5e7993a6fdfbaea287e9627e8e986602742bebed
ispdls <is.pdls90@gmail.com>
2024-09-30 21:25:26 -0400

    function the ope a file to read

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+import os
+
+"""function to read a file"""
+
+
+def read_file(filename=""):
+    """open the file to read"""    
+    with open(filename, "r", encoding="utf-8") as myfile:
+        print(myfile.read())
