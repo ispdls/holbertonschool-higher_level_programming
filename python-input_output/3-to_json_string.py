a66d60131edf5614e64f1b8d69989a5d717dc3ab
ispdls <is.pdls90@gmail.com>
2024-09-30 23:02:54 -0400

    function that returns the JSON representation of an object (string):

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+
+"""function that returns the JSON representation of an object (string):"""
+
+
+def to_json_string(my_obj):
+    """"
+    args:
+    my_obj: The object to convert to JSON format.
+
+    return:
+    data:  A string containing the JSON representation of the object.
+    """
+
+    data = json.dumps(my_obj)
+    return data
