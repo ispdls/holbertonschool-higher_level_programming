b6c49f4e72b9c3301f3762eeba817c277fced399
ispdls <is.pdls90@gmail.com>
2024-10-06 21:34:44 -0400

    retrieves a dictionary representation of a Student instance

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pytho3
+"""class Student that defines a student"""
+
+
+class Student:
+    """Initialize the student instance"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieve a dictionary representation of a Student instance."""
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+         }
