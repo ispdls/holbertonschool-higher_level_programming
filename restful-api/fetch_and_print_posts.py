71f3fd7cdcdd392082e2f8222a1805ac7b3e76bc
ispdls <is.pdls90@gmail.com>
2024-10-17 21:49:51 -0400

    post

--- restful-api/fetch_and_print_posts.py
+++ restful-api/fetch_and_print_posts.py
@@ -7,7 +7,6 @@ that fetches all post from JSONPlaceholder."""
 
 def fetch_and_print_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(r)
     if r.ok:
         datas = r.json()
 
